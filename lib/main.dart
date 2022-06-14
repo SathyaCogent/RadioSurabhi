643c382c07831521541fde95ec76ce69136ce088
SathyaCogent <104661109+SathyaCogent@users.noreply.github.com>
2022-06-14 10:01:24 +0530

    user alert version updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:assets_audio_player/assets_audio_player.dart' as assetply;
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
@@ -6,15 +6,16 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:new_version/new_version.dart';
 import 'package:radiosurabhi/resources/api/api_base_helper.dart';
 import 'package:radiosurabhi/ui/route_generator.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:workmanager/workmanager.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
-
-import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:upgrader/upgrader.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 class MyHttpOverrides extends HttpOverrides {
   @override
@@ -25,7 +26,7 @@ class MyHttpOverrides extends HttpOverrides {
   }
 }
 
-void callbackDispatcher() {
+/*void callbackDispatcher() {
   Workmanager().executeTask((task, inputData) async {
     var dataResponse = await ApiBaseHelper().get('im/v1/news');
 
@@ -56,42 +57,57 @@ void callbackDispatcher() {
 
     return Future.value(true);
   });
+}*/
+Future<void> _messageHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('Handling a background message ${message.messageId}');
 }
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_messageHandler);
   HttpOverrides.global = MyHttpOverrides();
   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
   String initialRoute = '/';
 
-  SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  final NotificationAppLaunchDetails? notificationAppLaunchDetails =
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight
+  ]);
+
+  /*final NotificationAppLaunchDetails? notificationAppLaunchDetails =
       await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
   if (notificationAppLaunchDetails?.didNotificationLaunchApp ?? false) {
     initialRoute = '/newspage';
-  }
-  await flutterLocalNotificationsPlugin.initialize(
+  }*/
+  /* await flutterLocalNotificationsPlugin.initialize(
       const InitializationSettings(
           android: AndroidInitializationSettings('@mipmap/ic_launcher'),
           iOS: IOSInitializationSettings(),
           macOS: MacOSInitializationSettings()),
-      onSelectNotification: (String? payload) => selectNotification(payload));
-  Workmanager().initialize(callbackDispatcher, isInDebugMode: false);
-  Workmanager().registerPeriodicTask("5", 'simplePeriodicTask',
+      onSelectNotification: (String? payload) => selectNotification(payload));*/
+  //Workmanager().initialize(callbackDispatcher, isInDebugMode: false);
+  /*Workmanager().registerPeriodicTask("5", 'simplePeriodicTask',
       existingWorkPolicy: ExistingWorkPolicy.replace,
       initialDelay: const Duration(seconds: 0),
       frequency: const Duration(hours: 23),
       constraints: Constraints(
         networkType: NetworkType.not_required,
-      ));
+      ));*/
   runApp(MyApp(
     initialRoute: initialRoute,
   ));
-  /*SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((_) {
+  /*SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight
+  ]).then((_) {
     runApp(
       DevicePreview(
         enabled: !kReleaseMode,
@@ -107,10 +123,10 @@ class MyApp extends StatefulWidget {
   const MyApp({Key? key, this.initialRoute = '/'}) : super(key: key);
 
   final String initialRoute;
-
-  static AssetsAudioPlayer audioPlayer = AssetsAudioPlayer();
-  static ValueNotifier<PlayerState> playerstate =
-      ValueNotifier<PlayerState>(PlayerState.pause);
+  static bool isshowflash = true;
+  static assetply.AssetsAudioPlayer audioPlayer = assetply.AssetsAudioPlayer();
+  static ValueNotifier<assetply.PlayerState> playerstate =
+      ValueNotifier<assetply.PlayerState>(assetply.PlayerState.pause);
   @override
   State<MyApp> createState() => _MyAppState();
 }
@@ -120,48 +136,63 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
   static FirebaseAnalyticsObserver observer =
       FirebaseAnalyticsObserver(analytics: analytics);
   String url = 'https://radiosurabhi.streamguys1.com/live1';
+  late FirebaseMessaging messaging;
   void setupPlaylist() async {
-    MyApp.audioPlayer.open(Audio.liveStream(url),
-        showNotification: false,
-        playInBackground: PlayInBackground.enabled,
-        audioFocusStrategy: AudioFocusStrategy.request(
+    MyApp.audioPlayer.open(assetply.Audio.liveStream(url),
+        showNotification: true,
+        playInBackground: assetply.PlayInBackground.enabled,
+        audioFocusStrategy: assetply.AudioFocusStrategy.request(
             resumeAfterInterruption: true, resumeOthersPlayersAfterDone: true),
         //forceOpen: true,
-        // notificationSettings: NotificationSettings(
-        //     seekBarEnabled: false,
-        //     stopEnabled: true,
-        //     prevEnabled: false,
-        //     nextEnabled: false,
-        //     playPauseEnabled: true,
-        //     customStopAction: (player) {
-        //       print('erttert');
-        //       MyApp.audioPlayer.stop();
-        //     },
-        //     // notificationSettings: NotificationSettings(
-        //     //     nextEnabled: false,
-        //     //     prevEnabled: false,
-        //     //     playPauseEnabled: true,
-        //     //     stopEnabled: true,
-        //     //     customStopAction: (players) {
-        //     //       print('2222222222');
-        //     //     },
-        //     customPlayPauseAction: (players) {
-        //       print('---fddddfff');
-        //       if (players.isPlaying.value) {
-        //         MyApp.audioPlayer.stop();
-        //       } else {
-        //         MyApp.audioPlayer.play();
-        //       }
-        //     }),
+        notificationSettings: const assetply.NotificationSettings(
+          seekBarEnabled: false,
+          stopEnabled: true,
+          prevEnabled: false,
+          nextEnabled: false,
+          playPauseEnabled: true,
+        ),
         autoStart: false);
+  }
+
+  checkSplashScreen() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool _seen = (prefs.getBool('seen') ?? false);
   }
 
   @override
   void initState() {
     super.initState();
+    checkSplashScreen();
     setupPlaylist();
 
-    print('-ee---------ccccc');
+    FirebaseMessaging.instance.getInitialMessage();
+    FirebaseMessaging.onMessage.listen((RemoteMessage event) async {
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+          FlutterLocalNotificationsPlugin();
+      await flutterLocalNotificationsPlugin.initialize(
+          const InitializationSettings(
+              android: AndroidInitializationSettings('@mipmap/ic_notii'),
+              iOS: IOSInitializationSettings(),
+              macOS: MacOSInitializationSettings()),
+          onSelectNotification: (String? payload) =>
+              selectNotification(payload));
+
+      AndroidNotificationDetails androidPlatformChannelSpecifics =
+          const AndroidNotificationDetails(
+              'your channel id', 'your channel name',
+              importance: Importance.max,
+              priority: Priority.high,
+              showWhen: false);
+      NotificationDetails platformChannelSpecifics =
+          NotificationDetails(android: androidPlatformChannelSpecifics);
+      await flutterLocalNotificationsPlugin.show(0, event.notification!.title,
+          event.notification!.body, platformChannelSpecifics,
+          payload: null);
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((message) async {
+      Navigator.pushNamed(context, '/newspage');
+    });
   }
 
   @override
@@ -170,13 +201,13 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
       useInheritedMediaQuery: true,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
-      title: 'Drawer Menu',
+      title: 'Radio Surabhi',
       debugShowCheckedModeBanner: false,
       navigatorObservers: <NavigatorObserver>[observer],
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: widget.initialRoute,
+      initialRoute: MyApp.isshowflash ? widget.initialRoute : '/homepage',
       navigatorKey: NavigationService.navigatorKey,
       onGenerateRoute: RouteGenerator.generateRoute,
     );
